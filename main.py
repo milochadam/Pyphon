7bb5368e930afac1082072f942c274e99de67921
milochadam <miloch.adam@gmail.com>
2020-11-14 19:44:30 +0100

    Added a switch to get american transcription

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from settings import COLOR_CACHED, COLOR_NOT_FOUND, COLOR_SCRAPED, DATABASE_PATH
 class PyphonArgs(argparse.Namespace):
     sentence: List[str] = field(default_factory=list)
     verbose: bool = False
+    american: bool = False
+    british: bool = False
 
 
 def get_args() -> PyphonArgs:
@@ -24,11 +26,18 @@ def get_args() -> PyphonArgs:
         "--sentence", "-s", type=str, nargs='+', help='A sentence to transcript',
     )
     parser.add_argument("--verbose", "-v", action='store_true', help='Vebose')
+    parser.add_argument("--american", "-a", action='store_true', help='American pronunciation')
+    parser.add_argument("--british", "-b", action='store_true', help='British pronunciation (default)')
     return parser.parse_args(namespace=PyphonArgs())
 
 
 def main() -> int:
     args = get_args()
+
+    if args.american and args.british:
+        print('Flags -a and -b are mutually exclusive')
+        return 1
+
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
     logging.debug("Instantiating a database in `%s`", DATABASE_PATH)
@@ -40,14 +49,16 @@ def main() -> int:
     for word in args.sentence:
         try:
             [candidate] = db.get_all(word)
-            result.append(colored(candidate.br, COLOR_CACHED))
+            word_transcription = candidate.am if args.american else candidate.br
+            result.append(colored(word_transcription, COLOR_CACHED))
         except ValueError:
             item = CambridgeScraper().scrape(word)
             if item is None:
                 result.append(colored(word, COLOR_NOT_FOUND))
             else:
                 db.insert(item)
-                result.append(colored(item.br, COLOR_SCRAPED))
+                word_transcription = item.am if args.american else item.br
+                result.append(colored(word_transcription, COLOR_SCRAPED))
     logging.debug('Result: %s', result)
 
     print(' '.join(result))
